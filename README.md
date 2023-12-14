77b7eb00dae92654598fbfcf99afcf4a19992ba2
camargo <gui.ferreira.camargo@gmail.com>
2023-12-14 11:57:22 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+## About me Page
+
+Site que apresenta meus projetos com foco na aplicação do pré-processador de CSS LESS. 
+Com uso de Mixins, Maps, Escapings, variáveis e divisão dos arquivos. E utilização do Boostrap para
+criação de Modal como formulário e jQuery para validação de campos.
--- js/main.js
+++ js/main.js
@@ -1,55 +1,32 @@
 $(document).ready(function() {
-    $('#telefone').mask('(00) 00000-0000');
-
     $("form").on('submit', function(event) {
         event.preventDefault();
 
-        $('#sucesso').hide();
-        
         $("form").validate({
             rules: {
                 nome: {
                     required: true,
                 },
                 email: {
-                    required: false,
+                    required: true,
                     email: true,
                 },
-                telefone: {
+                mensagem: {
                     required: true,
-                },
-                usuario: {
-                    required: true,
-                },
-                senha: {
-                    required: true,
-                },
-                senha2: {
-                    required: true,
-                    equalTo: '#senha',
-                },
-                checkbox: {
-                    required: true,
-                },
+                }
                 
             },
             messages: {
                 nome: 'Por favor, digite seu nome',
-                telefone: 'Por favor, digite seu telefone',
-                usuario: 'Por favor, digite um usuário',
-                senha: 'Por favor, digite uma senha',
-                senha2: 'Por favor, a senha deve ser igual',
-                checkbox: 'Por favor, marque este campo',
+                email: 'Por favor, digite seu e-mail',
+                mensagem: 'Por favor, digite uma mensagem'
             },
         
             submitHandler: function() {
-                $('#sucesso').show();
+                $('#mensagem-sucesso').show();
                 $('#nome').val('');
                 $('#email').val('');
-                $('#telefone').val('');
-                $('#usuario').val(''); 
-                $('#senha').val('');
-                $('#senha2').val('');
+                $('#mensagem').val('');
             }
         })
     })
